7fb57102b4a0b11f9cf07091b1122defe05c05ab
Kasper Karstensen <kasper_karstensen@hotmail.com>
2013-02-26 09:02:02 +0100

    update

---
+++ src/java/dk/cphbusiness/choir/view/CommandException.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.cphbusiness.choir.view;
+
+/**
+ *
+ * @author Kasper
+ */
+public class CommandException extends Exception {
+    
+}
